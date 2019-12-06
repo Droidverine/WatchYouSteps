31da9989b445880cc5fcb2498abe8c64adbd9c8b
ShivrajPatil <shivrajmanik.patil@student.griffith.ie>
2019-12-06 11:45:50 +0000

    Bracket 3 Partially completed, Log of each run are being calculated accurately.

--- app/src/main/java/com/droidverine/watchyousteps/MainActivity.java
+++ app/src/main/java/com/droidverine/watchyousteps/MainActivity.java
@@ -17,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
@@ -25,23 +26,29 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class MainActivity extends AppCompatActivity {
     private LocationManager locationManager;
     LocationListener locationListener;
-    Chronometer chronometer;
+    Chronometer chronometer, chronometerkm;
     String langlong;
-    TextView lc;
+    TextView lc, TxttotalRun, Txtavgspeed, Txtavgspeedperkm;
+    long eachkm;
+    Double elapsed;
     Button btnstart, btnstop;
     static double distanceInMetres;
     static Location lastLocation = null;
+    long millistart, millisend;
 
     //Distance to km= distance * 1000
     //Avg speed= distance/time.
@@ -52,9 +59,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         lc = findViewById(R.id.locationttxt);
         setSupportActionBar(toolbar);
+        chronometerkm = findViewById(R.id.chronometerperkm);
         chronometer = findViewById(R.id.chronometer);
         btnstart = findViewById(R.id.btnstart);
         btnstop = findViewById(R.id.btnstop);
+        TxttotalRun = findViewById(R.id.txttotalrun);
+        Txtavgspeed = findViewById(R.id.txtavgspeed);
         locationListener = LocServices();
         btnstop.setVisibility(View.GONE);
 
@@ -65,11 +75,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (locationManager != null) {
-                    locationManager.requestLocationUpdates("gps", 0, 100, locationListener);
+                    locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
                     btnstart.setVisibility(View.GONE);
                     btnstop.setVisibility(View.VISIBLE);
                     chronometer.setBase(SystemClock.elapsedRealtime());
                     chronometer.start();
+                    millistart = System.currentTimeMillis();
+                    //  Date dt = new Date(dtMili);
+                    Log.d("GetTime", "" + (millistart));
                 }
 
             }
@@ -79,23 +92,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (locationManager != null) {
+                    chronometerkm.start();
                     locationManager.removeUpdates(locationListener);
                     // locationManager = null;
                     lc.setText("");
                     btnstop.setVisibility(View.GONE);
                     btnstart.setVisibility(View.VISIBLE);
-                    Double elapsed = Double.valueOf(SystemClock.elapsedRealtime() - chronometer.getBase());
+                    elapsed = Double.valueOf(SystemClock.elapsedRealtime() - chronometer.getBase());
+                    //Distance to km= distance * 1000
                     Double avghr = ((elapsed / 1000) / 3600);
                     Log.d("chrono Second", "" + (elapsed / 1000));
                     Log.d("chrono hour", "" + avghr);
                     Log.d("chrono distance in km", "" + distanceInMetres / 1000);
-
+                    //Avg speed= distanceinkm/timeinhr.
+                    Double avgspeed = (distanceInMetres / 1000) / avghr;
+                    Log.d("chrono speed", "" + avgspeed);
                     Date currentTime = Calendar.getInstance().getTime();
                     File f = new File(getFilesDir(), currentTime + ".txt");
+
                     try {
                         BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-                        bw.write("Date and Time \t" + currentTime+"\n");
+                        bw.write("Date and Time \t" + currentTime + "\n");
                         bw.write("Distance Travelled \t" + distanceInMetres / 1000 + "\n");
+                        bw.write("Avg Speed \t" + avgspeed);
+
                         bw.write("Time Elapsed \t" + avghr);
 
                         Log.d("ZALA", "GHE");
@@ -103,6 +123,9 @@ public class MainActivity extends AppCompatActivity {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    TxttotalRun.setText("" + distanceInMetres / 1000);
+                    Txtavgspeed.setText("" + avgspeed);
+
                     distanceInMetres = 0;
 
                     chronometer.stop();
@@ -173,12 +196,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onLocationChanged(Location location) {
                 langlong = String.valueOf(location.getLatitude());
+                Log.d("loc", "currentloc " + location);
+                Log.d("loc", "locationali " + lastLocation);
 
 
-                if (lastLocation != null && lastLocation != location) {
+                if (lastLocation != null) {
                     distanceInMetres += location.distanceTo(lastLocation);
-                    Log.d("loc", "locationali" + distanceInMetres);
+                    Log.d("loc", "locationali " + distanceInMetres);
+                    eachkm = Math.round(distanceInMetres / 1000);
+                    //to get every km
+                    if (distanceInMetres >= 1000) {
+                        Log.d("km complete", " km complete");
+                        eachkm = 0;
+                        millisend = System.currentTimeMillis();
+                        //  Date dt = new Date(dtMili);
+                        elapsed = Double.valueOf(SystemClock.elapsedRealtime() - chronometer.getBase());
+
+                        Log.d("GetTime", "" + 1/(millisend - millistart) / 1000);
+                      //  Log.d("GetTime", "" + elapsed / 1000);
+                        // Log.d("chrono per km hour", "" + avghr);
+                        // chronometerkm.stop();
+
+                    }
+
                     lc.setText("" + (distanceInMetres / 1000));
+
                 }
                 lastLocation = location;
 
@@ -199,6 +241,7 @@ public class MainActivity extends AppCompatActivity {
             public void onProviderDisabled(String s) {
                 if (s.equals(LocationManager.GPS_PROVIDER)) {
                     requestforgps();
+                    lastLocation=null;
                 }
 
 
@@ -224,6 +267,40 @@ public class MainActivity extends AppCompatActivity {
                 });
         final AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    void Timermethod() {
+        Timer t = new Timer();
+        //Set the schedule function and rate
+        t.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //TextView tv = (TextView) findViewById(R.id.main_timer_text);
+                        // tv.setText(String.valueOf(minutes)+":"+String.valueOf(seconds));
+                        //seconds -= 1;
+
+                        //    if(seconds == 0)
+                        //  {
+                        //    tv.setText(String.valueOf(minutes)+":"+String.valueOf(seconds));
+
+                        //  seconds=60;
+                        // minutes=minutes-1;
+
+                        //    }
+
+
+                    }
+
+                });
+            }
+
+        }, 0, 1000000);
+
     }
 
 
